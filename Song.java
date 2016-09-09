0d9017a057e5e87ff9fc44f36b0b1e3b0d60b94e
Peter.YE <peter.ye.datawind@gmail.com>
2016-09-09 10:12:23 -0400

    init commit

---
+++ Song.java
@@ -0,0 +1,31 @@
+import java.io.*
+public class Song{
+    ArrayList<Song> = new ArrayList<Song>(); 
+    int bpm;
+    String title;
+
+
+    public static void main(String[] argc)
+    {
+        Song s = new Song();
+        s.getData();
+        System.io.prinln("hello");
+
+    }
+
+    /* read song.txt, get token, put in the array list */
+    public String getData(){
+        File io = new File("song.txt");
+        inputStream is = new inputStream();
+        BufferedReader line = is.readLine();
+
+        
+
+    }
+
+    public String getToken(){
+        
+
+    }
+
+}
