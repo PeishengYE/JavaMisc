e0552efc9bb7be8cf5df8dde3b3520986821cd08
Peter.YE <peter.ye.datawind@gmail.com>
2016-09-09 10:53:59 -0400

    added working code

--- Song.java
+++ Song.java
@@ -1,6 +1,7 @@
-import java.io.*
+import java.io.*;
+import java.util.*;
 public class Song{
-    ArrayList<Song> = new ArrayList<Song>(); 
+    ArrayList<String> songList = new ArrayList<String>(); 
     int bpm;
     String title;
 
@@ -9,21 +10,36 @@ public class Song{
     {
         Song s = new Song();
         s.getData();
-        System.io.prinln("hello");
+        System.out.println("hello");
 
     }
 
     /* read song.txt, get token, put in the array list */
-    public String getData(){
-        File io = new File("song.txt");
-        inputStream is = new inputStream();
-        BufferedReader line = is.readLine();
+    public void getData(){
+
+
+        try{
+
+        File file = new File("song.txt");
+        BufferedReader reader = new BufferedReader( new FileReader(file));
+
+        String line = null;
+        while ((line = reader.readLine()) != null)
+            addLine(line);
+
+
+        }catch (Exception ex){
+            ex.printStackTrace();
+
+        }
 
         
 
     }
 
-    public String getToken(){
+    public void addLine(String lineToParse){
+
+        System.out.println(lineToParse);
         
 
     }
